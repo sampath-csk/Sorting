ce1e2c9320425ae3e0beed5a198d4b94ae0e8aea
sampa <sampa@LAPTOP-OOBG8I07>
2020-02-11 13:19:56 +0530

    merge branch commit for sorting

--- src/lab4/NewSort.java
+++ src/lab4/NewSort.java
@@ -1,10 +1,35 @@
 package lab4;
 
+import java.util.Arrays; 
 public class NewSort {
+ static void bubbleSort(int arr[], int n) 
+		    {
+		        if (n == 1) 
+		            return; 
+		       
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		        for (int i=0; i<n-1; i++) 
+		            if (arr[i] > arr[i+1]) 
+		            { 
+		                int temp = arr[i]; 
+		                arr[i] = arr[i+1]; 
+		                arr[i+1] = temp; 
+		            } 
+		     
+		        bubbleSort(arr, n-1); 
+		    } 
+		      
+		    public static void main(String[] args) 
+		    { 
+		        int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+		       
+		        bubbleSort(arr, arr.length); 
+		          
+		        System.out.println("Sorted array : "); 
+		        System.out.println(Arrays.toString(arr)); 
+		    } 
+		
 
 	}
 
-}
+
